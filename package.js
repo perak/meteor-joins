866617fd404a032be6187af463524b567cb8fb7e
Petar Korponaić <petar.korponaic@gmail.com>
2017-08-03 06:44:01 +0200

    Fixed bug related to reactive joins

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: "perak:joins",
 	summary: "Generic collection joins for Meteor",
-	version: "1.1.1",
+	version: "1.1.2",
 	git: "https://github.com/perak/meteor-joins.git"
 });
 
