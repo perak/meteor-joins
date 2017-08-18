39739c26ee4837bc7b7ae00606ff8523b5d62e56
Petar Korponaić <petar.korponaic@gmail.com>
2017-08-18 04:50:47 +0200

    Improved reactive joins

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: "perak:joins",
 	summary: "Generic collection joins for Meteor",
-	version: "1.1.5",
+	version: "1.1.6",
 	git: "https://github.com/perak/meteor-joins.git"
 });
 
