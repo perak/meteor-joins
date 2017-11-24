087dc0d49546f2c7e3dd8673d2f4f3c52992ac8c
Petar Korponaić <petar.korponaic@gmail.com>
2017-11-25 00:25:15 +0100

    If joined doc is not found, set container to empty object instead null

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: "perak:joins",
 	summary: "Generic collection joins for Meteor",
-	version: "1.1.6",
+	version: "1.1.7",
 	git: "https://github.com/perak/meteor-joins.git"
 });
 
